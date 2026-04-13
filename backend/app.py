4f2ca1895f3cdd11daef1643911be026b06f27f1
Derek Frederick Dsouza <derekdesouza1310@gmail.com>
2026-04-13 15:11:22 +0530

    Prepared backend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -166,5 +166,10 @@ def predict_input():
 # =========================
 # RUN APP
 # =========================
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 if __name__ == "__main__":
     app.run(debug=True)
